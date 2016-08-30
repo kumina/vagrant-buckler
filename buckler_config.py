d0d415f3a8dd2861a09314466aeb666c9e376a13
Ed Schouten <ed@kumina.nl>
2016-08-30 15:21:28 +0200

    Keep up with config file changes.
    This now makes Buckler work again in Vagrant.

--- buckler_config.py
+++ buckler_config.py
@@ -4,16 +4,22 @@ def hash_passwd(s):
     return crypt.crypt(s, '$1$xyz')
 
 CONFIG = { 'customera': {'password': hash_passwd('secreta'),
-                   'indexes': ('customera-*',)},
+                   'indexes': ('customera-*',),
+                   'userdata_index': 'customera'},
            'customerb': {'password': hash_passwd('secretb'),
-                    'indexes': ('customerb-*', )},
+                    'indexes': ('customerb-*', ),
+                    'userdata_index': 'customerb'},
            'customerc': {'password': hash_passwd('secretc'),
-                    'indexes': ('customerc-*', )},
+                    'indexes': ('customerc-*', ),
+                    'userdata_index': 'customerc'},
            'kumina': {'password': hash_passwd('secretk'),
-                         'indexes':('customera-*', 'customerb-*', 'customerc-*')},
+                         'indexes':('customera-*', 'customerb-*', 'customerc-*'),
+                         'userdata_index': 'kumina'},
            'superuser': {'password': 'geheim',
-                         'indexes':('customera-*', 'customerb-*', 'customerc-*')}
+                         'indexes':('customera-*', 'customerb-*', 'customerc-*'),
+                         'userdata_index': 'superuser'},
          }
 
 KIBANA_UPSTREAM = "http://localhost:5601"
 ES_UPSTREAM = "http://localhost:9200"
+ES_USERDATA_PREFIX = '.kibana'
